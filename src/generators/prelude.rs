9fb1ad1b45feaaad55dd78a63d294c8a71350966
IAMMRGODIE <103867697+IAMMRGODIE@users.noreply.github.com>
2026-01-26 15:37:34 +0800

    Delete src directory
    moving src into `i_am_dsp`

--- src/generators/prelude.rs
+++
@@ -1,7 +0,0 @@
-//! Prelude module for the generators module.
-
-pub use super::sampler::*;
-pub use super::wavetable::*;
-pub use super::adsr::*;
-pub use super::additive::*;
-pub use super::stereo_generator::*;
